0f3d91144c999e1bbdde36cc8bf911e319d27f88
Milica Calija <calijamilica94@gmail.com>
2025-09-29 18:21:25 +0200

    Ispravljena greška createConnection i ostale izmene

--- database.js
+++ database.js
@@ -1,20 +1,15 @@
 require('dotenv').config();
-
 const mysql = require('mysql2');
-// Kreiraj konekciju ka bazi
-const db = mysql.createConnection({
-  //Oba se uvode jedan za lokalno drugi za jeavno testiranje
-  host: process.env.DB_HOST || process.env.MYSQLHOST,
-  port: process.env.DB_PORT || process.env.MYSQLPORT,
-  user: process.env.DB_USER || process.env.MYSQLUSER,
-  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
-  database: process.env.DB_NAME || process.env.MYSQLDATABASE
-});
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
 
-// Povezivanje
-db.connect((err) => {
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(err => {
   if (err) {
     console.error('❌ Ne mogu da se povežem na bazu:', err);
     return;
@@ -23,3 +18,4 @@ db.connect((err) => {
 });
 
 module.exports = db;
+
